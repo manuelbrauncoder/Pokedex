d0f870782732d396425efbfc33e715ca5f6e87ba
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-22 20:50:57 +0200

    start styling detail view

--- src/app/poke-details/poke-details.component.ts
+++ src/app/poke-details/poke-details.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { ApiService } from '../services/api.service';
+import { CommonModule } from '@angular/common';
+import { ZeroPadPipe } from '../pipes/zero-pad.pipe';
 
 @Component({
   selector: 'app-poke-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ZeroPadPipe],
   templateUrl: './poke-details.component.html',
   styleUrl: './poke-details.component.scss',
 })
