a0a2a58f2a9eefff6ee8744560171f280a5279a8
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-30 21:40:59 +0200

    implement imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MaincontentComponent } from './maincontent/maincontent.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 export const routes: Routes = [
-    { path: '', component: MaincontentComponent }
+    { path: '', component: MaincontentComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy', component: PrivacyComponent },
 ];
