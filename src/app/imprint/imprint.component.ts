b1a52d987cb8e35a7547afb2f5ab97bbf469f243
Manuel Braun <manuel.braun.coder@gmail.com>
2024-07-31 16:51:39 +0200

    style imprint, add back btn

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MaincontentComponent } from '../maincontent/maincontent.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
 
+  private router = inject(Router);
+
+  goBack(){
+    this.router.navigate(['/']);
+    setTimeout(() => {
+      window.scrollTo({top: 0});
+    }, 50);
+  }
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  private router = inject(Router);
 
+  goToComponent(component: string){
+    this.router.navigate([component]);
+    setTimeout(() => {
+      window.scrollTo({top: 0})
+    }, 50);
+  }
 }
